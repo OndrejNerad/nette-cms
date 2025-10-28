29247ec256feed9ee4726977930b01fdef669c3b
ondrej.nerad <ondrej.nerad@movisio.com>
2025-10-28 20:40:23 +0100

    added StaticPagePres - basic but works

--- app/Core/RouterFactory.php
+++ app/Core/RouterFactory.php
@@ -15,7 +15,10 @@ final class RouterFactory
 	public static function createRouter(): RouteList
 	{
 		$router = new RouteList;
-		$router->addRoute('<presenter>/<action>[/<id>]', 'Home:default');
+        $router->addRoute('/', 'StaticPage:default');
+        $router->addRoute('<url>', 'StaticPage:default');
+
+//        $router->addRoute('<presenter>/<action>[/<id>]', 'Home:default');
 		return $router;
 	}
 }

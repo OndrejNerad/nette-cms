7d1129d5801752b649c6e18f65c1e40e33280d27
Ondřej Nerad <ondrej.nerad>
2025-12-07 13:23:12 +0100

    FIXED ROUTERgit status!

--- app/Core/RouterFactory.php
+++ app/Core/RouterFactory.php
@@ -6,16 +6,26 @@ namespace App\Core;
 
 use Nette;
 use Nette\Application\Routers\RouteList;
+use Nette\Application\Routers\Route;
 
 final class RouterFactory
 {
-	use Nette\StaticClass;
-
     public static function createRouter(): RouteList
     {
         $router = new RouteList;
-        $router->addRoute('/', 'StaticPage:default');
-        $router->addRoute('<url>', 'StaticPage:default');
+
+        // Optional: exact home page (so / is also handled nicely)
+        $router->addRoute('', [
+            'presenter' => 'StaticPage',
+            'action'     => 'default',
+            'url'        => 'home', // or whatever your home latte is called
+        ]);
+
+        // This is the magic line – catches everything except real files/folders
+        $router->addRoute('<url .+>', [  // the ".+" means "at least one character"
+            'presenter' => 'StaticPage',
+            'action'     => 'default',
+        ]);
 
         return $router;
     }

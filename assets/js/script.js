4a04fdb2b878c4bfdb2651678e55c90ed43f7709
Ondřej Nerad <ondrej.nerad>
2025-12-09 23:54:47 +0100

    respo hp

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,11 +20,16 @@ $(function() {
      * PRODUCTS CAROUSEL
      */
     const productsSwiper = new Swiper('.product-carousel', {
-        slidesPerView: 1.2,
+        slidesPerView: 1,
         spaceBetween: 43,
         loop: false,
         pagination: {
             el: '.product-pagination'
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 1.2
+            }
         }
     });
 
@@ -33,12 +38,58 @@ $(function() {
      * REVIEWS CAROUSEL
      */
     const reviewsSwiper = new Swiper('.review-carousel', {
-        slidesPerView: 3.2,
+        slidesPerView: 1,
         spaceBetween: 43,
         loop: false,
+        breakpoints: {
+            568: {
+                slidesPerView: 1.5
+            },
+            992: {
+                slidesPerView: 2
+            },
+            1200: {
+                slidesPerView: 2.5
+            },
+            1400: {
+                slidesPerView: 3.2
+            }
+        }
     });
 
 
-    console.log('end of script');
+    /**
+     * WE OFFER CAROUSEL
+     */
+    const weOfferSwiper = new Swiper('.we-offer-carousel', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: false,
+        breakpoints: {
+            680: {
+                slidesPerView: 1.5
+            }
+        }
+    });
 
+
+    /**
+     * SOCIAL CAROUSEL
+     */
+    const socialSwiper = new Swiper('.social-carousel', {
+        slidesPerView: 1.5,
+        spaceBetween: 20,
+        loop: false,
+        breakpoints: {
+            680: {
+                slidesPerView: 2
+            },
+            992: {
+                slidesPerView: 2.5
+            },
+            1100: {
+                slidesPerView: 3
+            }
+        }
+    });
 });
